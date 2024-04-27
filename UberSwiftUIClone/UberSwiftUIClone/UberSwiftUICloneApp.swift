c3e044d2af4ded0303a181ac59316a231c00f0bb
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-04-27 12:12:16 +0300

    init project

---
+++ UberSwiftUIClone/UberSwiftUIClone/UberSwiftUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUICloneApp.swift
+//  UberSwiftUIClone
+//
+//  Created by Oleksandr Isaiev on 27.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
