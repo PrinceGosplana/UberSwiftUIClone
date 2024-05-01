120e54c0215a694467b9febcbb17ee7687551672
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-05-01 15:38:33 +0300

    ask user location permission and update map

---
+++ UberSwiftUIClone/UberSwiftUIClone/Managers/LocationManager.swift
@@ -0,0 +1,28 @@
+//
+//  LocationManager.swift
+//  UberSwiftUIClone
+//
+//  Created by Oleksandr Isaiev on 01.05.2024.
+//
+
+import CoreLocation
+
+final class LocationManager: NSObject, ObservableObject {
+
+    private let locationManager = CLLocationManager()
+
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard !locations.isEmpty else { return }
+        locationManager.stopUpdatingLocation()
+    }
+}
