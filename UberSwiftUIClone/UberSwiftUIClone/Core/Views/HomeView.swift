0338445a54f61063a3a1362cbd9760b1fbda05b3
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-05-01 14:57:38 +0300

    organise project

---
+++ UberSwiftUIClone/UberSwiftUIClone/Core/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  UberSwiftUIClone
+//
+//  Created by Oleksandr Isaiev on 01.05.2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
