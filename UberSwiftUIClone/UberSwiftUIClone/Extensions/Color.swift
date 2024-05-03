e4c737210320026d7411420ac651f7df64321a19
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-05-03 17:43:46 +0300

    add color extension for support dark and light modes

---
+++ UberSwiftUIClone/UberSwiftUIClone/Extensions/Color.swift
@@ -0,0 +1,16 @@
+//
+//  Color.swift
+//  UberSwiftUIClone
+//
+//  Created by Oleksandr Isaiev on 03.05.2024.
+//
+
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let backgroundColor = Color("BackgroundColor")
+}
